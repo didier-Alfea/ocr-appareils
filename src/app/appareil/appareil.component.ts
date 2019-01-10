f06150f8f60e4323e9841cf3e5c7fad983e41116
didier-Alfea <Didier.CLIN@alfea-consulting.com>
2019-01-10 18:07:03 +0100

    first commit

---
+++ src/app/appareil/appareil.component.ts
@@ -0,0 +1,39 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { AppareilService } from '../services/appareil.service';
+
+@Component({
+  selector: 'app-appareil',
+  templateUrl: './appareil.component.html',
+  styleUrls: ['./appareil.component.scss']
+})
+export class AppareilComponent implements OnInit {
+
+  @Input() appareilName: string;
+  @Input() appareilStatus: string;
+  @Input() indexOfAppareil: number;
+
+  constructor(private appareilService: AppareilService) { }
+
+  ngOnInit() {
+  }
+
+  getStatus() {
+    return this.appareilStatus;
+  }
+
+  getColor() {
+    if (this.appareilStatus === "Allumé") {
+      return "green";
+    }
+    else if (this.appareilStatus === "Eteint")
+      {return "red";}
+  }
+
+  onSwitchOne() {
+    this.appareilService.switchOnOne(this.indexOfAppareil);
+  }
+
+  offSwitchOne() {
+    this.appareilService.switchOffOne(this.indexOfAppareil);
+  }
+}
---
+++ src/app/services/appareil.service.ts
@@ -0,0 +1,28 @@
+export class AppareilService {
+  appareils = [
+    { name: 'Machine à laver',
+    status: 'Eteint' },
+    { name: 'Lave vaisselle',
+    status: 'Allumé' },
+    { name: 'Micro onde',
+    status: 'Eteint' }
+  ];
+
+  switchOnAll() {
+    for(let appareil of this.appareils) {
+      appareil.status = "Allumé";
+    }
+  }
+  switchOffAll() {
+    for(let appareil of this.appareils) {
+      appareil.status = "Eteint";
+    }
+  }
+
+  switchOnOne(index: number) {
+    this.appareils[index].status = "Allumé";
+  }
+  switchOffOne(index: number) {
+    this.appareils[index].status = "Eteint";
+  }
+}
