774fe5ca274d09aecbc76bb6475df7ce767616bf
didier-Alfea <Didier.CLIN@alfea-consulting.com>
2019-01-11 11:29:15 +0100

    Ajout rxjs pour les observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  secondes: number;
 
   constructor() { }
 
-}
+  ngOnInit() {
+    const counter = Observable.interval(1000);
+    counter.subscribe(
+       (value: number) => {
+         this.secondes = value;
+       },
+       (error: any) => {
+         console.log('Une erreur a été rencontrée');
+       },
+       () => {
+         console.log('Observable complétée !')
+       }
+     )
+   }
+ }
