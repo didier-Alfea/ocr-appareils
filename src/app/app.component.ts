74b06a93465662f237e17290f23103bbd62fc2e8
didier-Alfea <Didier.CLIN@alfea-consulting.com>
2019-01-11 12:04:10 +0100

    Observable par objet Subscription pour eviter pb comp infini

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/interval';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
 
   secondes: number;
+  // Ajout d'un objet de type subscription
+  counterSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
     const counter = Observable.interval(1000);
-    counter.subscribe(
-       (value: number) => {
-         this.secondes = value;
-       },
-       (error: any) => {
-         console.log('Une erreur a été rencontrée' + error);
-       },
-       () => {
-         console.log('Observable complétée !');
-       }
-     )
+    this.counterSubscription = counter.subscribe(
+      (value: number) => {
+        this.secondes = value;
+      }
+    )
    };
+
+   ngOnDestroy() {
+     this.counterSubscription.unsubscribe();
+   }
  }
